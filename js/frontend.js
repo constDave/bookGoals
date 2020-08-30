aacf946b1e4170e9b18399028aec98690344fb9b
Dave Enriquez <const.dave@gmail.com>
2020-08-29 19:59:18 -0500

    Adding frontend logic for the add a new book form

--- js/frontend.js
+++ js/frontend.js
@@ -1,41 +1,92 @@
+const form = document.querySelector("form");
+const formBookTitle = document.querySelector("#bookTitle");
+const formWordGoals = document.querySelector("#wordGoals");
+const formDeadline = document.querySelector("#deadline");
+const booksDiv = document.querySelector("#books");
 
+const serverUrl = "http://localhost:3000/addbook";
 
-const form = document.querySelector('form')
-const serverUrl = 'http://localhost:3000/addbook'
+form.addEventListener("submit", e => {
+  e.preventDefault();
 
+  const formData = new FormData(form);
+  const bookTitle = formData.get("bookTitle");
+  const wordGoals = formData.get("wordGoals");
+  const deadline = formData.get("deadline");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const formData = new FormData(form)    
-    const bookTitle = formData.get('bookTitle')
-    const wordGoals = formData.get('wordGoals')
-    const deadline = formData.get('deadline')
-    
-    const bookEntry = {
-        bookTitle,
-        wordGoals,
-        deadline
+  if (formBookTitle.value === "" || formWordGoals.value === "") {
+    alert("Fields are required.");
+  }
+
+  const bookEntry = {
+    bookTitle,
+    wordGoals,
+    deadline: new Date(deadline)
+  };
+
+  async function postData(url, data = {}) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  }
+
+  postData(serverUrl, bookEntry)
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+  console.log(bookEntry);
+  formBookTitle.value = "";
+  formWordGoals.value = "";
+  document.querySelector("#books").innerHTML = "";
+  getBooks(serverUrl);
+});
+
+async function getBooks(url) {
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const data = await response.json();
+    const bookEntries = Array.from(data);
+    createElements(bookEntries);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createElements(data) {
+  data.forEach(book => {
+    const card = document.createElement("div");
+    const cardBody = document.createElement("div");
+    const wordsToComplete = document.createElement("div");
+    const dateDue = document.createElement("div");
+    dateDue.classList.add("col-md-6");
+    dateDue.innerHTML = `Your deadline: ${book.deadline}`;
+    wordsToComplete.classList.add("col-md-6");
+    wordsToComplete.innerHTML = `Total Words Goal: ${book.wordgoals}`;
+    card.classList.add("card");
+    cardBody.classList.add("card-body");
+    let cardTitle = document.createElement("h4");
+    cardTitle.classList.add("text-center");
+    cardTitle.innerHTML = `${book.booktitle}`;
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(wordsToComplete);
+    if (book.deadline !== null) {
+      cardBody.appendChild(dateDue);
     }
- 
-    async function postData(url, data = {}){
-        const response = await fetch(url, {
-            method: 'POST', 
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-        return response.json()
-    }
+    console.log(wordsToComplete);
 
-    postData(serverUrl, bookEntry)
-    .then(data => console.log(data)
-    ).catch(error => console.error(error)
-    )
+    card.appendChild(cardBody);
+    booksDiv.appendChild(card);
+  });
+}
 
-    console.log(bookEntry);
-    
-
-
-    
-})
+getBooks(serverUrl);
